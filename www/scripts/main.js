58aff1140435dda83f7aab14a45c434ac8b5f816
KrissiBT <kristofer@kristofer.is>
2018-12-03 23:01:59 +0000

    cmd update maybe????

--- www/scripts/main.js
+++ www/scripts/main.js
@@ -38,7 +38,7 @@ function makeGold() {
     console.log(copper);
   }
   else {
-      $("#sleep1").text("You Need to Sleep!!");
+      cmd("You need to rest before begging any further");
   }
 }
 
@@ -71,4 +71,10 @@ function eat() {
     $("#hunger").text(hunger);
     $( "#Food" ).text(food);
   }
+}
+//=================================== Cmd log function ========================
+function cmd(log) {
+  $(".cmd").find('p:first').remove();
+  $(".cmd").append("<p id='cmdText'></p>");
+  $(".cmd").find('p:last').text("# " + log);
 }

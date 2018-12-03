c07e2ea70ea0500f6b9d4debed3c7fe62256a1f3
luztez <emelsi15@gmail.com>
2018-12-03 23:10:42 +0000

    Revert "cmd update"
    This reverts commit 68b97513a624d4b61966df1c584e305a9030b499.

--- www/scripts/main.js
+++ www/scripts/main.js
@@ -27,9 +27,7 @@ function makeGold() {
     copper = parseInt($( "#copper" ).text());
     //============ random begging chance ===========
     if (Math.random() * 100 > 50) {
-      var begReward = parseInt(Math.random() * 5);
-      cmd("A stranger took pity on you and gave you " + begReward + " copper");
-      copper += begReward;// got some beg
+      copper += 1;// got some beg
     }
     fatigue += 10; // make you more sleepy
     hunger -= 1; // make you more hungry
